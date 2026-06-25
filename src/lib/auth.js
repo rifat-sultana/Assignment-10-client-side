247e45e545a97b92617fe7ddc5189e5888c7b257
Rifat Sultana <89746461+rifat-sultana@users.noreply.github.com>
2026-06-25 22:19:23 +0600

    Refactor auth.js for better authentication handling
    Refactor authentication setup to use a createAuth function and lazy initialization.

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,20 +2,22 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
+let _auth;
 
-const client = new MongoClient(process.env.MONGO_DB_URI);
-const db = client.db(process.env.AUTH_DB_NAME);
+function createAuth() {
+  const client = new MongoClient(process.env.MONGO_DB_URI);
+  const db = client.db(process.env.AUTH_DB_NAME);
+  return betterAuth({
+    emailAndPassword: {
+      enabled: true,
+    },
+    database: mongodbAdapter(db, { client }),
+  });
+}
 
-
-
-export const auth = betterAuth({
-  //...other options
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
-
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
-  }),
-});
+export function getAuth() {
+  if (!_auth) {
+    _auth = createAuth();
+  }
+  return _auth;
+}
